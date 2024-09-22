b6aa1e37a3345ae4a7375ad3728de3159d295288
Vik Praet <vik.praet@gmail.com>
2024-09-22 22:38:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === 'production' ? '/Portfolio/' : '/',  // Use '/Portfolio/' for GitHub Pages
+}));
